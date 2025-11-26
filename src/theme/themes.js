b837c86024c49e27ce61a69df77f403f6a4b631a
Katarzyna Maculewicz <qla.katarzyna@gmail.com>
2025-11-26 12:00:15 +0000

    Add theme colors and light/dark theme configurations

---
+++ src/theme/themes.js
@@ -0,0 +1,25 @@
+export const colors = {
+  white: "#ffffff",
+  iron: "#fbfbfeff",
+  mercury:"#e5e5e5",
+  slateGray:"#6E7E91",
+  oxfordBlue:"#e5e5e572",
+  mineShaft:"#252525",
+  black: "#000000"
+};
+
+export const lightTheme = {
+  background: colors.iron,
+  text: colors.slateGray,
+
+  toggleBackground: colors.mercury,
+  icon: colors.white,
+};
+
+export const darkTheme = {
+  background: colors.mineShaft,
+  text: colors.white,
+  icon: colors.black,
+
+  toggleBackground: colors.oxfordBlue,
+};
